f76eb855951c306184a1665de20b1a734987b323
megagao <megagao@126.com>
2016-08-23 20:42:00 +0800

    使用spring JdbcTemplate替换了原来的jdbc代码

--- src/main/java/org/hqu/indoor_pos/rmi/EnvFactorManageImpl.java
+++ src/main/java/org/hqu/indoor_pos/rmi/EnvFactorManageImpl.java
@@ -2,22 +2,24 @@ package org.hqu.indoor_pos.rmi;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.hqu.indoor_pos.bean.EnvFactor;
 import org.hqu.indoor_pos.server.Server;
-import org.hqu.indoor_pos.util.DBUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.RowMapper;
 
 public class EnvFactorManageImpl extends UnicastRemoteObject implements EnvFactorManage{
 
 	private static final long serialVersionUID = 1L;
 
-	private Connection conn = DBUtil.getConnection();
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	
 	public EnvFactorManageImpl() throws RemoteException {
 		super();
@@ -29,20 +31,15 @@ public class EnvFactorManageImpl extends UnicastRemoteObject implements EnvFacto
 	@Override
 	public List<EnvFactor> findAllEnvFactor() throws RemoteException {
 		
-		List<EnvFactor> envFactors = new ArrayList<EnvFactor>();
-		
-		try {
-			PreparedStatement stat; 
-			stat = conn.prepareStatement("select * from env_factor");
-			ResultSet rs = stat.executeQuery();
-			while(rs.next()){
-				EnvFactor envFactor = new EnvFactor(rs.getInt(1), rs.getDouble(2), rs.getDouble(3), rs.getDouble(4));
-				envFactors.add(envFactor);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return envFactors;
+		return this.jdbcTemplate.query("select * from env_factor",   
+                new RowMapper<EnvFactor>(){  
+              
+                    @Override  
+                    public EnvFactor mapRow(ResultSet rs, int rowNum) throws SQLException {  
+                    	EnvFactor envFactor = new EnvFactor(rs.getInt(1), rs.getDouble(2), rs.getDouble(3), rs.getDouble(4));
+                        return envFactor;  
+                    }  
+        });  
 	}
 
 	/**
@@ -53,19 +50,17 @@ public class EnvFactorManageImpl extends UnicastRemoteObject implements EnvFacto
 	public EnvFactor findEnvFactorByRoomId(Integer roomId)
 			throws RemoteException {
 
-		EnvFactor envFactor = null;
-		
-		try {
-			PreparedStatement stat; 
-			stat = conn.prepareStatement("select * from env_factor where room_id = ?");
-			stat.setInt(1, roomId);
-			ResultSet rs = stat.executeQuery();
-			rs.next();
-			envFactor = new EnvFactor(rs.getInt(1), rs.getDouble(2), rs.getDouble(3), rs.getDouble(4));
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return envFactor;
+		return (EnvFactor) this.jdbcTemplate.queryForObject(  
+                "select * from env_factor where room_id = ?",   
+                new Object[]{roomId},  
+                new RowMapper<EnvFactor>(){  
+  
+                    @Override  
+                    public EnvFactor mapRow(ResultSet rs,int rowNum)throws SQLException {  
+                    	EnvFactor envFactor = new EnvFactor(rs.getInt(1), rs.getDouble(2), rs.getDouble(3), rs.getDouble(4));
+                        return envFactor;  
+                    }  
+        }); 
 	}
 
 	/**
@@ -76,15 +71,13 @@ public class EnvFactorManageImpl extends UnicastRemoteObject implements EnvFacto
 	public boolean saveEnvFactor(EnvFactor envFactor)
 			throws RemoteException {
 		
+		this.jdbcTemplate.update("insert into env_factor (room_id, height, atten_factor, p0) values (?, ?, ?, ?)",   
+                new Object[]{envFactor.getRoomId(), envFactor.getHeight(), envFactor.getN(), envFactor.getP0()}); 
+		
 		try {
-			PreparedStatement stat; 
-			stat = conn.prepareStatement("insert into env_factor (room_id, height, atten_factor, p0) values (?, ?, ?, ?)");
-			stat.setInt(1, envFactor.getRoomId());
-			stat.setDouble(2, envFactor.getHeight());
-			stat.setDouble(3, envFactor.getN());
-			stat.setDouble(4, envFactor.getP0());
-			stat.executeUpdate();
-		} catch (SQLException e) {
+			this.jdbcTemplate.update("insert into env_factor (room_id, height, atten_factor, p0) values (?, ?, ?, ?)",   
+	                new Object[]{envFactor.getRoomId(), envFactor.getHeight(), envFactor.getN(), envFactor.getP0()}); 
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
@@ -97,18 +90,23 @@ public class EnvFactorManageImpl extends UnicastRemoteObject implements EnvFacto
 	 * @param envFactor
 	 */
 	@Override
-	public boolean updateEnvFactor(EnvFactor envFactor)
+	public boolean updateEnvFactor(final EnvFactor envFactor)
 			throws RemoteException {
 		
 		try {
-			PreparedStatement stat; 
-			stat = conn.prepareStatement("update env_factor set height = ?, atten_factor = ?, p0 = ? where room_id = ?");
-			stat.setDouble(1, envFactor.getHeight());
-			stat.setDouble(2, envFactor.getN());
-			stat.setDouble(3, envFactor.getP0());
-			stat.setInt(4, envFactor.getRoomId());
-			stat.executeUpdate();
-		} catch (SQLException e) {
+			this.jdbcTemplate.update(  
+					"update env_factor set height = ?, atten_factor = ?, p0 = ? where room_id = ?",   
+	                new PreparedStatementSetter(){  
+	                    @Override  
+	                    public void setValues(PreparedStatement ps) throws SQLException {  
+	                        ps.setDouble(1, envFactor.getHeight());
+	                        ps.setDouble(2, envFactor.getN());
+	                        ps.setDouble(3, envFactor.getP0());
+	                        ps.setInt(4, envFactor.getRoomId());
+	                    }  
+	                }  
+	        );  
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
@@ -121,14 +119,19 @@ public class EnvFactorManageImpl extends UnicastRemoteObject implements EnvFacto
 	 * @param roomId
 	 */
 	@Override
-	public boolean deleteEnvFactor(Integer roomId) throws RemoteException {
+	public boolean deleteEnvFactor(final Integer roomId) throws RemoteException {
 		
 		try {
-			PreparedStatement stat; 
-			stat = conn.prepareStatement("delete from env_factor  where room_id = ?");
-			stat.setInt(1, roomId);
-			stat.executeUpdate();
-		} catch (SQLException e) {
+			this.jdbcTemplate.update(  
+					"delete from env_factor  where room_id = ?",   
+	                new PreparedStatementSetter(){  
+	                    @Override  
+	                    public void setValues(PreparedStatement ps) throws SQLException {  
+	                        ps.setInt(1, roomId); 
+	                    }  
+	                }  
+	        );  
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
