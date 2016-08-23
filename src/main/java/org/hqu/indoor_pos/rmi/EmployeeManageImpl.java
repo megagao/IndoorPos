99da6579e370b3cfc09464df3edc6ed57eff26a6
megagao <megagao@126.com>
2016-08-23 20:41:28 +0800

    删除了没用到的引入包

--- src/main/java/org/hqu/indoor_pos/rmi/EmployeeManageImpl.java
+++ src/main/java/org/hqu/indoor_pos/rmi/EmployeeManageImpl.java
@@ -7,7 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import org.hqu.indoor_pos.bean.BaseStation;
 import org.hqu.indoor_pos.bean.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
