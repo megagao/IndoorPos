d54ade728bf8daffc48c6c6a7cfa1c668257ce38
megagao <megagao@126.com>
2016-08-24 10:34:37 +0800

    使用spring修改rmi服务接口

--- src/main/java/org/hqu/indoor_pos/rmi/LoginInfoManage.java
+++ src/main/java/org/hqu/indoor_pos/rmi/LoginInfoManage.java
@@ -1,40 +1,38 @@
 package org.hqu.indoor_pos.rmi;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
 import java.util.List;
 
 import org.hqu.indoor_pos.bean.LoginUser;
 
-public interface LoginInfoManage extends Remote {
+public interface LoginInfoManage {
 
 	/**
 	 * 查找所有用户信息
 	 */
-	public List<LoginUser> findAllLoginUser() throws RemoteException; 
+	public List<LoginUser> findAllLoginUser(); 
 	
 	/**
 	 * 保存用户
 	 * @param loginUser
 	 */
-	public boolean saveLoginUser(LoginUser loginUser) throws RemoteException;
+	public boolean saveLoginUser(LoginUser loginUser);
 	
 	/**
 	 * 修改用户
 	 * @param loginUser
 	 */
-	public boolean updateLoginUser(LoginUser loginUser) throws RemoteException;
+	public boolean updateLoginUser(LoginUser loginUser);
 	
 	/**
 	 * 删除用户
 	 * @param userId
 	 */
-	public boolean deleteLoginUser(String userId) throws RemoteException;
+	public boolean deleteLoginUser(String userId);
 	
 	/**
 	 * 根据用户名查询用户
 	 * @param userId
 	 */
-	public LoginUser getLoginUserById(String userId) throws RemoteException;
+	public LoginUser getLoginUserById(String userId);
 	
 }
