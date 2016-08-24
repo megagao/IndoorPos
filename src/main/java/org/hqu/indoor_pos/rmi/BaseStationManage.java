7c4d1b12e0007d90af3c51dee33b586be0eca0be
megagao <megagao@126.com>
2016-08-24 10:22:44 +0800

    使用spring修改rmi服务接口

--- src/main/java/org/hqu/indoor_pos/rmi/BaseStationManage.java
+++ src/main/java/org/hqu/indoor_pos/rmi/BaseStationManage.java
@@ -1,46 +1,44 @@
 package org.hqu.indoor_pos.rmi;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
 import java.util.List;
 
 import org.hqu.indoor_pos.bean.BaseStation;
 
-public interface BaseStationManage extends Remote {
+public interface BaseStationManage {
 	
 	/**
 	 * 查找所有基站
 	 */
-	public List<BaseStation> findAllBaseStation() throws RemoteException; 
+	public List<BaseStation> findAllBaseStation(); 
 
 	/**
 	 * 根据房间id查找房间内的基站
 	 * @param roomId
 	 */
-	public List<BaseStation> findBaseStationByRoomId(Integer roomId) throws RemoteException; 
+	public List<BaseStation> findBaseStationByRoomId(Integer roomId); 
 	
 	/**
 	 * 保存基站
 	 * @param baseStation
 	 */
-	public boolean saveBaseStation(BaseStation baseStation) throws RemoteException;
+	public boolean saveBaseStation(BaseStation baseStation);
 	
 	/**
 	 * 修改基站信息
 	 * @param baseStation
 	 */
-	public boolean updateBaseStation(BaseStation baseStation) throws RemoteException;
+	public boolean updateBaseStation(BaseStation baseStation);
 	
 	/**
 	 * 删除基站
 	 * @param baseId
 	 */
-	public boolean deleteBaseStation(String baseId) throws RemoteException;
+	public boolean deleteBaseStation(String baseId);
 	
 	/**
 	 * 根据基站id查询基站
 	 * @param baseId
 	 */
-	public BaseStation getBaseStationById(String baseId) throws RemoteException;
+	public BaseStation getBaseStationById(String baseId);
 	
 }
