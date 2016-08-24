25b17f2d28948faa4d5e42d615a722709a52c3a9
megagao <megagao@126.com>
2016-08-24 10:38:41 +0800

    使用spring修改rmi服务接口

--- src/main/java/org/hqu/indoor_pos/rmi/RoomManage.java
+++ src/main/java/org/hqu/indoor_pos/rmi/RoomManage.java
@@ -1,51 +1,49 @@
 package org.hqu.indoor_pos.rmi;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
 import java.util.List;
 
 import org.hqu.indoor_pos.bean.RoomInfo;
 
-public interface RoomManage extends Remote {
+public interface RoomManage {
 	
 	/**
 	 * 查找所有房间信息
 	 */
-	public List<RoomInfo> findAllRoomInfo() throws RemoteException; 
+	public List<RoomInfo> findAllRoomInfo(); 
 	
 	/**
 	 * 得到房间列表
 	 */
-	public List<String> findRoomList() throws RemoteException; 
+	public List<String> findRoomList(); 
 	
 	/**
 	 * 根据roomName查询房间信息
 	 * @param roomName  
 	 */
-	public RoomInfo getRoomInfoByName(String roomName) throws RemoteException; 
+	public RoomInfo getRoomInfoByName(String roomName); 
 	
 	/**
 	 * 根据roomID查询房间信息
 	 * @param roomId
 	 */
-	public RoomInfo getRoomInfoById(Integer roomId) throws RemoteException;
+	public RoomInfo getRoomInfoById(Integer roomId);
 	
 	/**
 	 * 保存房间信息
 	 * @param roomInfo
 	 */
-	public boolean saveRoomInfo(RoomInfo roomInfo) throws RemoteException;
+	public boolean saveRoomInfo(RoomInfo roomInfo);
 	
 	/**
 	 * 修改房间信息
 	 * @param roomInfo
 	 */
-	public boolean updateRoomInfo(RoomInfo roomInfo) throws RemoteException;
+	public boolean updateRoomInfo(RoomInfo roomInfo);
 	
 	/**
 	 * 删除房间信息
 	 * @param roomId
 	 */
-	public boolean deleteRoomInfo(Integer roomId) throws RemoteException;
+	public boolean deleteRoomInfo(Integer roomId);
 
 }
