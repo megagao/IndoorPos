2dfe503c7714d916acd12a637af5a681935268c5
megagao <megagao@126.com>
2016-08-24 10:25:52 +0800

    使用spring修改rmi服务接口

--- src/main/java/org/hqu/indoor_pos/rmi/Login.java
+++ src/main/java/org/hqu/indoor_pos/rmi/Login.java
@@ -1,16 +1,13 @@
 package org.hqu.indoor_pos.rmi;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
 import org.hqu.indoor_pos.bean.LoginUser;
 
-public interface Login extends Remote{
+public interface Login {
 	
 	/**
 	 * 登录
 	 * @param str
 	 */
-	public LoginUser login(String str) throws RemoteException; 
+	public LoginUser login(String str); 
 	
 }
