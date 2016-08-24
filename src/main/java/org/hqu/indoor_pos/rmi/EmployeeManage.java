048ba7a9976d5bd9a876d66e550e1295478bf65d
megagao <megagao@126.com>
2016-08-24 10:27:50 +0800

    使用spring修改rmi服务接口

--- src/main/java/org/hqu/indoor_pos/rmi/EmployeeManage.java
+++ src/main/java/org/hqu/indoor_pos/rmi/EmployeeManage.java
@@ -1,40 +1,38 @@
 package org.hqu.indoor_pos.rmi;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
 import java.util.List;
 
 import org.hqu.indoor_pos.bean.Employee;
 
-public interface EmployeeManage extends Remote {
+public interface EmployeeManage {
 
 	/**
 	 * 查找所有员工
 	 */
-	public List<Employee> findAllEmp() throws RemoteException; 
+	public List<Employee> findAllEmp(); 
 	
 	/**
 	 * 保存员工
 	 * @param employee
 	 */
-	public boolean saveEmployee(Employee employee) throws RemoteException;
+	public boolean saveEmployee(Employee employee);
 	
 	/**
 	 * 修改员工信息
 	 * @param employee
 	 */
-	public boolean updateEmployee(Employee employee) throws RemoteException;
+	public boolean updateEmployee(Employee employee);
 	
 	/**
 	 * 删除员工
 	 * @param empId
 	 */
-	public boolean deleteEmployee(String empId) throws RemoteException;
+	public boolean deleteEmployee(String empId);
 	
 	/**
 	 * 根据员工id查询员工
 	 * @param empId
 	 */
-	public Employee getEmployeeById(String empId) throws RemoteException;
+	public Employee getEmployeeById(String empId);
 	
 }
