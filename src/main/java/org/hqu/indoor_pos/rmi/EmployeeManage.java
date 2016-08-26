5dea05a94022305bdf6dc67475fb732b582b2a52
megagao <megagao@126.com>
2016-08-26 15:11:41 +0800

    修改了注释

--- src/main/java/org/hqu/indoor_pos/rmi/EmployeeManage.java
+++ src/main/java/org/hqu/indoor_pos/rmi/EmployeeManage.java
@@ -4,34 +4,52 @@ import java.util.List;
 
 import org.hqu.indoor_pos.bean.Employee;
 
+/**
+ * created on 2016年8月26日
+ *
+ * @description: 员工管理服务接口
+ *
+ * @author: megagao
+ * @version: 0.0.1
+ */
 public interface EmployeeManage {
 
 	/**
-	 * 查找所有员工
+	 * @description: 查找所有员工
+	 *
+	 * @return: 所有员工信息
 	 */
 	public List<Employee> findAllEmp(); 
 	
 	/**
-	 * 保存员工
-	 * @param employee
+	 * @description: 保存员工
+	 *
+	 * @param: employee
+	 * @return: 是否成功
 	 */
 	public boolean saveEmployee(Employee employee);
 	
 	/**
-	 * 修改员工信息
-	 * @param employee
+	 * @description: 修改员工信息
+	 *
+	 * @param: employee
+	 * @return: 是否成功
 	 */
 	public boolean updateEmployee(Employee employee);
 	
 	/**
-	 * 删除员工
-	 * @param empId
+	 * @description: 删除员工
+	 *
+	 * @param: empId
+	 * @return: 是否成功
 	 */
 	public boolean deleteEmployee(String empId);
 	
 	/**
-	 * 根据员工id查询员工
-	 * @param empId
+	 * @description: 根据员工id查询员工
+	 *
+	 * @param: empId
+	 * @return: 员工信息
 	 */
 	public Employee getEmployeeById(String empId);
 	
