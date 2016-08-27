292cb09b259f877a3b7fdf20f780884f514d5f33
megagao <megagao@126.com>
2016-08-27 21:22:53 +0800

    修改了注释

--- src/main/java/org/hqu/indoor_pos/rmi/Login.java
+++ src/main/java/org/hqu/indoor_pos/rmi/Login.java
@@ -2,11 +2,21 @@ package org.hqu.indoor_pos.rmi;
 
 import org.hqu.indoor_pos.bean.LoginUser;
 
+/**
+ * created on 2016年8月27日
+ *
+ * @description: 登录服务接口
+ *
+ * @author: megagao
+ * @version: 0.0.1
+ */
 public interface Login {
 	
 	/**
-	 * 登录
-	 * @param str
+	 * @description: 登录
+	 *
+	 * @param: str
+	 * @return: 登录人员
 	 */
 	public LoginUser login(String str); 
 	
