26c805c419bf92024719c57191910ad87b2cd661
megagao <megagao@126.com>
2016-08-27 10:13:35 +0800

    修改了注释

--- src/main/java/org/hqu/indoor_pos/rmi/EnvFactorManage.java
+++ src/main/java/org/hqu/indoor_pos/rmi/EnvFactorManage.java
@@ -4,34 +4,52 @@ import java.util.List;
 
 import org.hqu.indoor_pos.bean.EnvFactor;
 
+/**
+ * created on 2016年8月26日
+ *
+ * @description: 环境因子管理服务接口
+ *
+ * @author: megagao
+ * @version: 0.0.1
+ */
 public interface EnvFactorManage {
 
 	/**
-	 * 查找所有房间的环境因子
+	 * @description: 查找所有房间的环境因子
+	 *
+	 * @return: 所有房间的环境因子
 	 */
 	public List<EnvFactor> findAllEnvFactor(); 
 	
 	/**
-	 * 根据房间id查找相应的环境因子
-	 * @param roomId
+	 * @description: 根据房间id查找相应的环境因子
+	 *
+	 * @param: roomId
+	 * @return: 相应的环境因子
 	 */
 	public EnvFactor findEnvFactorByRoomId(Integer roomId); 
 	
 	/**
-	 * 保存环境因子
-	 * @param envFactor
+	 * @description: 保存环境因子
+	 *
+	 * @param: envFactor
+	 * @return: 是否成功
 	 */
 	public boolean saveEnvFactor(EnvFactor envFactor);
 	
 	/**
-	 * 修改环境因子
-	 * @param envFactor
+	 * @description: 修改环境因子
+	 *
+	 * @param: envFactor
+	 * @return: 是否成功
 	 */
 	public boolean updateEnvFactor(EnvFactor envFactor);
 	
 	/**
-	 * 删除环境因子
-	 * @param roomId
+	 * @description: 删除环境因子
+	 *
+	 * @param: roomId
+	 * @return: 是否成功
 	 */
 	public boolean deleteEnvFactor(Integer roomId);
 	
