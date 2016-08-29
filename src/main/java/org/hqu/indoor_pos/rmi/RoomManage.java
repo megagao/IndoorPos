0dbf7eeaab20b64489fd812227d02844f025f86c
megagao <megagao@126.com>
2016-08-29 14:44:34 +0800

    修改了注释

--- src/main/java/org/hqu/indoor_pos/rmi/RoomManage.java
+++ src/main/java/org/hqu/indoor_pos/rmi/RoomManage.java
@@ -4,45 +4,67 @@ import java.util.List;
 
 import org.hqu.indoor_pos.bean.RoomInfo;
 
+/**
+ * created on 2016年8月27日
+ *
+ * @description: 房间管理服务接口
+ *
+ * @author: megagao
+ * @version: 0.0.1
+ */
 public interface RoomManage {
 	
 	/**
-	 * 查找所有房间信息
+	 * @description: 查找所有房间信息
+	 *
+	 * @return: 所有房间信息
 	 */
 	public List<RoomInfo> findAllRoomInfo(); 
 	
 	/**
-	 * 得到房间列表
+	 * @description: 得到房间列表
+	 *
+	 * @return: 房间列表
 	 */
 	public List<String> findRoomList(); 
 	
 	/**
-	 * 根据roomName查询房间信息
-	 * @param roomName  
+	 * @description: 根据roomName查询房间信息
+	 *
+	 * @param： roomName
+	 * @return: 房间信息
 	 */
 	public RoomInfo getRoomInfoByName(String roomName); 
 	
 	/**
-	 * 根据roomID查询房间信息
-	 * @param roomId
+	 * @description: 根据roomID查询房间信息
+	 *
+	 * @param： roomId
+	 * @return: 房间信息
 	 */
 	public RoomInfo getRoomInfoById(Integer roomId);
 	
 	/**
-	 * 保存房间信息
-	 * @param roomInfo
+	 * @description: 保存房间信息
+	 *
+	 * @param: roomInfo
+	 * @return: 是否成功
 	 */
 	public boolean saveRoomInfo(RoomInfo roomInfo);
 	
 	/**
-	 * 修改房间信息
-	 * @param roomInfo
+	 * @description: 修改房间信息
+	 *
+	 * @param: roomInfo
+	 * @return: 是否成功
 	 */
 	public boolean updateRoomInfo(RoomInfo roomInfo);
 	
 	/**
-	 * 删除房间信息
-	 * @param roomId
+	 * @description: 删除房间信息
+	 *
+	 * @param: roomId
+	 * @return: 是否成功
 	 */
 	public boolean deleteRoomInfo(Integer roomId);
 
